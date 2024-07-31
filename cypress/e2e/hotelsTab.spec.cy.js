33ac3af32f99f29d5ac0794fb919873057148728
Nvasif <vasiffka@gmail.co>
2024-07-31 19:20:25 -0400

    text test

--- cypress/e2e/hotelsTab.spec.cy.js
+++ cypress/e2e/hotelsTab.spec.cy.js
@@ -8,14 +8,25 @@ describe('The sanity test suite for travel site', () => {
   
     it('check for tabs on each of the following items', () => {
       cy.visit('http://localhost:3000')
-      cy.get('#tab1 > form > :nth-child(1) > .input--style-1').should("be.visible")
-      cy.get('#input-start').should("be.visible")
-      cy.get('#input-end').should("be.visible")
-      cy.get('#info').should("be.visible")
-      cy.get('.checkbox-row > .m-r-45').should("be.visible").and("have.text", "add a flight")
+      cy.get('#tab1 > form > :nth-child(1) > .label').should("be.visible")
+      cy.get('#tab1 > form > .row > :nth-child(1) > .input-group > .label').should("be.visible")
+      cy.get('#tab1 > form > .row > :nth-child(2) > .input-group > .label').should("be.visible")
+      cy.get(':nth-child(3) > .label').should("be.visible")
+      cy.get('.checkbox-row > .m-r-45').should("be.visible")
       cy.get('.checkbox-row > :nth-child(2)').should("be.visible")
       cy.get('#tab1 > form > .btn-submit').should("be.visible")
     })
+
+    it('check for text of tabs on each of the following items', () => {
+        cy.visit('http://localhost:3000')
+        cy.get('#tab1 > form > :nth-child(1) > .label').should("have.text", "Going to")
+        cy.get('#tab1 > form > .row > :nth-child(1) > .input-group > .label').should("have.text", "check-in")
+        cy.get('#tab1 > form > .row > :nth-child(2) > .input-group > .label').should("have.text", "check-out")
+        cy.get(':nth-child(3) > .label').should("have.text", "travellers")
+        cy.get('.checkbox-row > .m-r-45').should("have.text", "Add a flight\n                                        \n                                        \n                                    ")
+        cy.get('.checkbox-row > :nth-child(2)').should("have.text", "Add a car\n                                        \n                                        \n                                    ")
+        cy.get('#tab1 > form > .btn-submit').should("have.text", "search")
+      })
 
   
   
